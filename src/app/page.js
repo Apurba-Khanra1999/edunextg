5d9f8d00058a0c0af7253b16b9c703c2d7cf548d
Apurba-Khanra1999 <apurbakhanra09@gmail.com>
2025-11-03 10:32:26 +0530

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,65 @@
+// import BannerOne from "@/components/banner/BannerOne";
+// import BlogOne from "@/components/blog/BlogOne";
+// import CaseStudies from "@/components/casestudies/CaseStudies";
+// import BackToTop from "@/components/common/BackToTop";
+// import CounterUp from "@/components/counterup/CounterUp";
+// import CtaOne from "@/components/cta/CtaOne";
+// import FaqOne from "@/components/faq/FaqOne";
+// import FooterOne from "@/components/footer/FooterOne";
+// import HeaderOne from "@/components/header/HeaderOne";
+// import LargeVideo from "@/components/large-video/LargeVideo";
+// import Pricing from "@/components/pricing/Pricing";
+// import ServiceOne from "@/components/services/ServiceOne";
+// import Testimonials from "@/components/testimonials/Testimonials";
+
+
+// export default function Home() {
+//   return (
+//     <div className='#'>
+//       <HeaderOne />
+//       <BannerOne />
+//       <LargeVideo />
+//       <ServiceOne />
+//       <CounterUp />
+//       <CaseStudies />
+//       <Pricing />
+//       <FaqOne />
+//       <Testimonials />
+//       <BlogOne />
+//       <CtaOne />
+//       <FooterOne />
+//       <BackToTop />
+//     </div>
+//   );
+// }
+
+
+
+import BannerFour from "@/components/banner/BannerFour";
+import BlogFour from "@/components/blog/BlogFour";
+import CaseFive from "@/components/casestudies/CaseFive";
+import BackToTop from "@/components/common/BackToTop";
+import CtaThree from "@/components/cta/CtaThree";
+import FooterThree from "@/components/footer/FooterThree";
+import HeaderOne from "@/components/header/HeaderOne";
+import ServiceFive from "@/components/services/ServiceFive";
+import ServiceFour from "@/components/services/ServiceFour";
+import TestimonialsFour from '@/components/testimonials/TestimonialsFour';
+
+export default function Home() {
+  return (
+    <div className='#'>
+      <HeaderOne />
+      <BannerFour />
+      <ServiceFour />
+      <ServiceFive />
+      <CaseFive />
+      <TestimonialsFour />
+      <BlogFour />
+      <CtaThree />
+      <FooterThree />
+      <BackToTop />
+    </div>
+  );
+}
+
